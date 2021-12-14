77bb03a910ce2d3467938d84c8271a5954110d14
Marko Nikolic <marko.supergun@gmail.com>
2021-12-14 11:08:05 +0100

    minify class

---
+++ src/minify.php
@@ -0,0 +1,22 @@
+<?php 
+
+namespace marko9827\minify;
+/*
+ * Minify Class
+ * @author Marko Nikolić <marko.supergun@gmail.com>
+ * @copyright 2021 Marko
+ * @license ../LICENCE.MD
+ * @link https://github.com/Marko9827/Phpminify
+ * @version 1.0.0
+ */
+class Minify {
+    function __construct() {
+
+
+    }
+    function isCode($string_or_file){
+        if(file_exists($string_or_file)){}
+    }
+}
+
+?>
