201daa0609818dc072ebefb7f1a58566b767ad35
Marko Nikolic <marko.supergun@gmail.com>
2021-12-14 11:56:17 +0100

    fix update3

--- src/minify.php
+++ src/minify.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 
 namespace marko9827\minify;
 /*
@@ -9,28 +9,39 @@ namespace marko9827\minify;
  * @link https://github.com/Marko9827/Phpminify
  * @version 1.0.0
  */
-class Minify {
-    private $type = true, $valid = false, $ext = "", $string_or_file = "";
-    function __construct() {
 
+class Minify
+{
+    private $type = true, $valid = false, $ext = "", $string_or_file = "", $return = "";
+    function __construct()
+    {
     }
-    function isCode($string_or_file){
+    function isCode($string_or_file)
+    {
         $this->setString_or_file($string_or_file);
-        if(file_exists($string_or_file)){
-
-        } 
+        if (file_exists($string_or_file)) {
+        }
+        return $this->getReturn();
     }
-      function fileScann(){
+
+    function fileScann()
+    {
         $this->setExt(pathinfo($this->getString_or_file(), PATHINFO_EXTENSION));
 
-        if($this->getExt() == ".css"){
-            
+        if ($this->getExt() == "css") {
         }
+        if ($this->getExt() == "js") {
+        }
+    }
+
+    function CSSM($content)
+    {
+        $this->setReturn(preg_replace('/(\/\*[\s\S]*?\*\/)|([\t\r\n])/', "", $content));
     }
 
     /**
      * Get the value of valid
-     */ 
+     */
     public function getValid()
     {
         return $this->valid;
@@ -40,7 +51,7 @@ class Minify {
      * Set the value of valid
      *
      * @return  self
-     */ 
+     */
     public function setValid($valid)
     {
         $this->valid = $valid;
@@ -50,7 +61,7 @@ class Minify {
 
     /**
      * Get the value of string_or_file
-     */ 
+     */
     public function getString_or_file()
     {
         return $this->string_or_file;
@@ -60,7 +71,7 @@ class Minify {
      * Set the value of string_or_file
      *
      * @return  self
-     */ 
+     */
     public function setString_or_file($string_or_file)
     {
         $this->string_or_file = $string_or_file;
@@ -70,7 +81,7 @@ class Minify {
 
     /**
      * Get the value of ext
-     */ 
+     */
     public function getExt()
     {
         return $this->ext;
@@ -80,7 +91,7 @@ class Minify {
      * Set the value of ext
      *
      * @return  self
-     */ 
+     */
     public function setExt($ext)
     {
         $this->ext = $ext;
@@ -90,7 +101,7 @@ class Minify {
 
     /**
      * Get the value of type
-     */ 
+     */
     public function getType()
     {
         return $this->type;
@@ -100,13 +111,31 @@ class Minify {
      * Set the value of type
      *
      * @return  self
-     */ 
+     */
     public function setType($type)
     {
         $this->type = $type;
 
         return $this;
     }
-}
 
-?>
+    /**
+     * Get the value of return
+     */
+    public function getReturn()
+    {
+        return $this->return;
+    }
+
+    /**
+     * Set the value of return
+     *
+     * @return  self
+     */
+    public function setReturn($return)
+    {
+        $this->return = $return;
+
+        return $this;
+    }
+}
